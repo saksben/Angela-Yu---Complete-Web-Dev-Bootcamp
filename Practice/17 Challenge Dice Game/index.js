6e5cd4df7cb0e6fe9385a16435920ff38bb370ff
Benjamin Saks <saksben2@gmail.com>
2024-03-19 19:46:19 -0700

    124 added flag to score

--- Practice/17 Challenge Dice Game/index.js
+++ Practice/17 Challenge Dice Game/index.js
@@ -13,9 +13,9 @@ die2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
 
 // Game logic for score
 if (randomNumber1 > randomNumber2) {
-    score.innerHTML = "Player 1 Wins!"
+    score.innerHTML = "🚩Player 1 Wins!"
 } else if (randomNumber1 < randomNumber2) {
-    score.innerHTML = "Player 2 Wins!"
+    score.innerHTML = "Player 2 Wins!🚩"
 } else {
     score.innerHTML = "Draw!"
 }

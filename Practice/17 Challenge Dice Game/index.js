84d3d1b3dbb49ac60f9cefc36655ef34e3ac7a52
Benjamin Saks <saksben2@gmail.com>
2024-03-19 19:43:24 -0700

    124 added game logic for score

--- Practice/17 Challenge Dice Game/index.js
+++ Practice/17 Challenge Dice Game/index.js
@@ -2,10 +2,20 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-// Selects first and second dice
+// Selects first and second dice, and score text
 let die1 = document.querySelectorAll("img")[0];
 let die2 = document.querySelectorAll("img")[1];
+let score = document.querySelectorAll("h1")[0];
 
 // Rolls both dice
 die1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
 die2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+// Game logic for score
+if (randomNumber1 > randomNumber2) {
+    score.innerHTML = "Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+    score.innerHTML = "Player 2 Wins!"
+} else {
+    score.innerHTML = "Draw!"
+}

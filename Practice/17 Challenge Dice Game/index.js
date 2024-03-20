d940684c35d06d540eee16d810109e2e26b78cbd
Benjamin Saks <saksben2@gmail.com>
2024-03-19 19:34:24 -0700

    124 added roll functionality for first die

--- Practice/17 Challenge Dice Game/index.js
+++ Practice/17 Challenge Dice Game/index.js
@@ -1,2 +1,8 @@
 // Generates a random number between 1 and 6
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+// Selects first die
+let die1 = document.querySelectorAll("img")[0]
+
+// Rolls the first die
+die1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
